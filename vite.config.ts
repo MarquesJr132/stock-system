82043b4a76de814a1b9ba9f120320c20005999c0
MarquesJr132 <marquesjr898@gmail.com>
2025-08-13 11:03:49 +0200

    Test 1

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/sistema-gestao-stock/' : '/',
+  base: mode === 'production' ? '/stock-system/' : '/',
   server: {
     host: "::",
     port: 8080,
